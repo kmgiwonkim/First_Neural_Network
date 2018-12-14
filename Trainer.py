4abf3802fcd6eed9d316b2f9d7d817e312e3c84d
kmgiwonkim <45646279+kmgiwonkim@users.noreply.github.com>
2018-12-14 03:17:57 -0500

    Update Trainer.py
    - Works!
    - Few improvements on the UI
    - Now also displays percentage of correctly recognized images

--- Trainer.py
+++ Trainer.py
@@ -1,10 +1,12 @@
 from mnist import MNIST
 import pickle
-import random
+from random import shuffle
 import numpy as np
 from pathlib import Path
+import time
 
 print('Setting up Variables', end='')
+
 
 class variables:
     
@@ -13,20 +15,20 @@ class variables:
         self.b = biases
 
 
-eta = 0.1 #learning rate
-batchSize = 15000
-repetitions = 100000
-n = np.array([784,32,10]) #Layers
+eta = 0.3 #learning rate (now independent on batchSize)
+batchSize = 1
+repetitions = 30
+n = np.array([784,28,10]) #Layers
 
 randrange = 1
 
 nRow = np.array([np.zeros((n[x],1)) for x in range(len(n))])#neuralRow
-zRow = np.array([np.zeros((n[x],1)) for x in range(len(n))])#neuralRow pre-sigmoid
+zRow = np.array([np.zeros((n[x],1)) for x in range(len(n))[1:]])#neuralRow pre-sigmoid
 
-w = np.array([np.zeros((n[x],n[x-1])) for x in range(len(n))[1:]]) #weights 
-b = np.array([np.zeros((n[x],1)) for x in range(len(n))[1:]]) #biases
+w1 = np.array([np.zeros((n[x],n[x-1])) for x in range(len(n))[1:]]) #weights 
+b1 = np.array([np.zeros((n[x],1)) for x in range(len(n))[1:]]) #biases
 
-var = variables(w,b)
+var = variables(w1,b1)
 
 delta = np.array([np.zeros((n[x],1)) for x in range(len(n))[1:]]) #error
 grad = np.array([ #gradient descent step
@@ -38,146 +40,163 @@ grad = np.array([ #gradient descent step
 mndata = MNIST('images')
 
 print('.', end='')
-images = np.transpose(mndata.load_training())
-#images = [np.array([np.array(images[0][x]),images[1][x]]) for x in range(len(images[0]))]
+images = mndata.load_training()
+images = [np.array([np.array(images[0][x]),images[1][x]]) for x in range(len(images[0]))]
+#CONSIDER USING 'zip()' INSTEAD
 print('.', end='')
 mndata = MNIST('images')
-imagesTe = np.transpose(mndata.load_testing())
-#imagesTe = [np.array([np.array(imagesTe[0][x]),imagesTe[1][x]]) for x in range(len(imagesTe[0]))]
+imagesTe = mndata.load_testing()
+imagesTe = [np.array([np.array(imagesTe[0][x]),imagesTe[1][x]]) for x in range(len(imagesTe[0]))]
 #This method was used because the array containing the pixel data has to be an np.array
+#There seems to be a bug where 'np.transpose()' seems to make every entry of 'images' the same
+#this was tested to be faster than 'np.transpose()'
+
 print('.', end='')
 
 
 
 costArr = np.array([
-    [np.zeros(n[len(n)-1]) for x in range(batchSize)]
+    [np.zeros(n[-1]) for x in range(batchSize)]
     for x in range(len(images)//batchSize)
     ])
 cost = np.array([
     np.zeros(batchSize) for x in range(len(images)//batchSize)
     ])
+costArrTot = np.array([
+    np.zeros(n[-1]) for x in range(len(images)//batchSize)
+    ])
 
-costArrTe = np.array([np.zeros(n[len(n)-1]) for x in range(len(imagesTe))])
+costArrTe = np.array([np.zeros(n[-1]) for x in range(len(imagesTe))])
 costTe = np.zeros(len(imagesTe))
 aveCost = 0
 prevCost = 0
 print(' Complete.')
+print('Layers:',n)
+print('Learning Rate:',eta)
+print('Batch Size:',batchSize)
 
 def sigmoidLimit(val): #function to avoid overflow. WARNING: may greatly reduce accuracy
     return 700*(sigmoid(val*2/350)-0.5)
 
 def sigmoidPrime(val):
-    #if any(np.exp(val)>100000):
-     #   print(np.exp(val))
-    return np.exp(val)/np.square(1.0+np.exp(sigmoidLimit(val)))
+    #sigmoid(z)*(1-sigmoid(z))
+    return np.array(np.exp(-val)/np.square(1.0+np.exp(-val)))
 
 def sigmoid(val):
-    #print(val)
-    return 1.0/(1.0+np.exp(-val))
+    return np.array(1.0/(1.0+np.exp(-val)))
 
-def generateRandVars():
-    wei = [np.array([randrange*round(random.random()*2-1,3) for x in range(n[x]*n[x+1])]).reshape((n[x+1],n[x])) for x in range(len(n)-1)] #weights
-    bia = [np.array([randrange*round(random.random()*2-1,3) for x in range(n[x+1])]).reshape((n[x+1],1)) for x in range(len(n)-1)] #biases
+def generateRandVars():#Generates a random set of weights and biases
+    wei = [np.random.randn(n[x+1],n[x]) for x in range(len(n)-1)] #weights
+    
+    bia = [np.random.randn(n[x+1],1) for x in range(len(n)-1)] #biases
     return bia,wei
 
-def SGD(p,b,w,grad):#StochasticGradientDescent
-    random.shuffle(images)
-    for batch in range(p//batchSize):
-
+def SGD(b,w,grad):#StochasticGradientDescent
+    shuffle(images)#Shuffle works properly
+    for batch in range(len(images)//batchSize):
         for index in range(batchSize):
-            arrrr = costFunction(images[batch*batchSize+index],b,w)
-            if len(arrrr) == 100:
-                print('Yellow',arrrr,index,batch)
-            costArr[batch][index] = arrrr
-            cost[batch][index] = float(sum(0.5*costArr[batch][index]**2))
-            bp = backProp(costArr[batch][index],grad,b,w)
-            grad += bp
-        #print(b,grad[0],'\n\n\n\n')
-        b = b - eta*grad[0]/sum(n)
-        w = w - eta*grad[1]/sum(n)
-        #print(b)
-        #print(len(w),len(w[0]),len(w[0][0]),len(w[1]),len(w[1][0]))
+            costArr[batch][index] = costFunction(images[batch*batchSize+index],b,w)
+            grad += backProp(costArr[batch][index],grad,b,w)
+        b = b - eta*grad[0]
+        w = w - eta*grad[1]
         grad = np.array([
             np.array([np.zeros((n[x],1)) for x in range(len(n))[1:]]),
             np.array([np.zeros((n[x],n[x-1])) for x in range(len(n))[1:]]) 
             ])
     return b,w
-    #print('\n\n\n###Gradient Descent Complete.###')
+
 
 def backProp(cost,grad,b,w):
-    #print('backprop',cost,zRow[len(n)-1])
-    delta[len(n)-2] = np.transpose([cost])*sigmoidPrime(zRow[len(n)-1])
-    #print(delta[len(n)-2])
-    
+    delta[-1] = np.transpose([cost])*sigmoidPrime(zRow[-1])
     for x in range(len(n))[len(n)-3::-1]:
-        delta[x] = np.matmul(np.transpose(w[x+1]),delta[x+1])*sigmoidPrime(zRow[x+1])
-    grad[0] = delta
+        delta[x] = np.matmul(np.transpose(w[x+1]),delta[x+1])*sigmoidPrime(zRow[x])
+    grad[0] = np.array(delta)
     for x in range(len(grad[1])):
-        grad[1][x] = np.ravel(nRow[x])*np.transpose([np.ravel(delta[x])])
+        grad[1][x] = np.array(np.ravel(nRow[x]) * delta[x])
     return grad
 
 
 def costFunction(img,b,w):
-    nRow[0] = np.array(img[0])/255.0
+    nRow[0] = np.transpose([img[0]])/255.0
     for x in range(len(n)-1):
-        m = np.matmul(w[x],nRow[x])
-        #print('cost',np.transpose([np.ravel(m)]),b[x])
-        zRow[x+1] = np.transpose([m])+b[x]
-        nRow[x+1] = sigmoid(zRow[x+1])
-    nRow[len(n)-1] = np.ravel(nRow[len(n)-1])
-    nRow[len(n)-1][img[1]] = 1.0 - nRow[len(n)-1][img[1]]
-    return nRow[len(n)-1]
+        zRow[x] = np.matmul(w[x],nRow[x])+b[x]
+        nRow[x+1] = sigmoid(zRow[x])
+    nRow[-1] = np.ravel(nRow[-1])
+    nRow[-1][img[1]] = nRow[-1][img[1]] - 1.0
+    return nRow[-1]
+
+
+def feedforward(img,b,w):
+    nRow[0] = np.transpose([img[0]])/255.0
+    for x in range(len(n)-1):
+        zRow[x] = np.matmul(w[x],nRow[x])+b[x]
+        nRow[x+1] = sigmoid(zRow[x])
+    nRow[-1] = np.ravel(nRow[-1])
+    return nRow[-1]
 
 
 def testAveCost(b,w):
-    #print(imagesTe[0],b,w)
     for index in range(len(imagesTe)):
         costArrTe[index] = 0.5*costFunction(imagesTe[index],b,w)**2.0
         costTe[index] = float(sum(costArrTe[index]))
-        '''for x in costArrTe[index]:
-            #print(x)
-            costTe[index] += float(x**2)'''
     return sum(costTe)/float(len(costTe))
+
+
+def testPercentAccuracy():
+    match = 0
+    for x in imagesTe:
+        Cc = feedforward(x,var.b,var.w)
+        if x[1] == np.nonzero(Cc == max(Cc))[0][0]:
+            match += 1
+    return match
 
 
 def trainVariables(Vname = 'Variables'):
     var.b,var.w = pickle.load(open('{}.p'.format(Vname),'rb'))
-    print('Imported "{}.p"'.format(Vname))
-    print('Calculating Initial Cost:')
-    aveCost = testAveCost(var.b,var.w)
-    print('Cost = {0}\n'.format(aveCost))
-    prevCost = aveCost
+    print('\n\n\n\nImported "{0}.p"\n'.format(Vname))
+    print('Layers:',n)
+    print('Learning Rate:',eta)
+    print('Batch Size:',batchSize)
+    print('Starting % Accuracy: {0:5.2f}%'.format(testPercentAccuracy()/len(imagesTe)*100))
     for x in range(repetitions):
         print('Gradient Descent {0:3}'.format(x+1), end='')
-        var.b,var.w = SGD(60000,var.b,var.w,grad) #Stochastic Gradient Descent
+        var.b,var.w = SGD(var.b,var.w,grad) #Stochastic Gradient Descent
         pickle.dump([var.b,var.w], open('{}.p'.format(Vname),'wb'))
         var.b,var.w = pickle.load(open('{}.p'.format(Vname),'rb'))
-        #print('Gradient Descent',x+1,(
-        print(' Complete.', end='')
-        costArrTe = np.array([np.zeros(10) for x in range(len(imagesTe))])
-        costTe = np.zeros(len(imagesTe))
-        aveCost = testAveCost(var.b,var.w)
-        print(
-            ' Cost = {0:16.14f} Cost Drop = {1}.'.format(
-                aveCost,prevCost - aveCost)
-            )
-        prevCost = aveCost
+
+        if 1:
+            print(' Complete:',end='')
+            print(' {0:5.2f}%'.format(testPercentAccuracy()/len(imagesTe)*100),end='')
+            print(' {0:7.5f}'.format(testAveCost(var.b,var.w)))
+        else:
+            print(' Complete.')
+        
 
 
-def loadUI():
+def UI():
     choice = None
+    print('\n\n 1 = Train Variables')
+    print(' 2 = Save Current Variables as...')
+    print(' 3 = Load Varible File...')
+    print(' 4 = Test Current Variables')
+    print(' 5 = Generate New Random Variables')
+    print(' d = Display List of Commands')
+    print(' q = Quit UI')
     while(choice != 'q'):
-        print('\n\n 1 = Train Variables')
-        print(' 2 = Save Current Variables as...')
-        print(' 3 = Load Varible File...')
-        print(' 4 = Test Current Variables')
-        print(' 5 = Generate New Random Variables')
-        print(' q = Quit UI')
         print('\nType Desired Function:',end = '')
         choice = input()
         
-        if choice not in ['1','2','3','4','5','q']:
+        if choice not in ['1','2','3','4','5','d','q']:
             print('Please do your shit properly')
+            
+        elif choice == 'd':
+            print('\n\n 1 = Train Variables')
+            print(' 2 = Save Current Variables as...')
+            print(' 3 = Load Varible File...')
+            print(' 4 = Test Current Variables')
+            print(' 5 = Generate New Random Variables')
+            print(' d = Display List of Commands')
+            print(' q = Quit UI')
             
         elif choice == '1':
             print('    Training Variables.')
@@ -199,40 +218,34 @@ def loadUI():
             name = input()
             if name == '0':
                 pickle.dump([var.b,var.w], open('Variables.p','wb'))
-                print('        Saved to Variables.p.')
+                print('        Saved to "Variables.p".')
             else:
                 pickle.dump([var.b,var.w], open('{}.p'.format(name),'wb'))
                 print('        Saved as {}.p.'.format(name))
             
     
         elif choice == '3':
-            print('Loading Varible File.')
-            print('Input file name (0 for main variables):',end = '')
+            print('    Loading Varible File.')
+            print('    Input file name (0 for main variables):',end = '')
             name = input()
             if name == '0':
                 var.b,var.w = pickle.load(open('Variables.p','rb'))
             else:
                 my_file = Path(name)
                 if my_file.is_file():
-                    var.b,var.w = pickle.load(open('{}.p'.format(name),'rb'))
+                    var.b,var.w = pickle.load(open('{}'.format(name),'rb'))
                 else:
                     print('    get your shit together')
         
         elif choice == '4':
-            print('    Test Current Variables')
-            random.shuffle(imagesTe)
-            Cc = 0.5*costFunction(imagesTe[0],var.b,var.w)**2
-            print('    Cost   =',sum(Cc))
-            print('    Number =',imagesTe[0][1])
-            print('    Array  =\n',Cc)
+            print('    Test Current Variables:',end='')
+            print(' {0:5.2f}%'.format(testPercentAccuracy()/len(imagesTe)*100))
     
         elif choice == '5':
             var.b,var.w = generateRandVars()
             print('    Generated New Random Variables.')
     
     
-loadUI()
-
-
+UI()
 
 
